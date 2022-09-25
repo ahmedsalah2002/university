d9767ec3b3509c7dee506456aef1ee4a3346cf71
abdoslg <abdoismen507@gmail.com>
2022-09-25 15:19:32 +0200

    all

---
+++ js/head.js
@@ -0,0 +1,15 @@
+let nav=document.querySelector("nav")
+window.addEventListener('scroll' ,function () {
+   if (window.scrollY >= 25) {
+      nav.classList.add("scroll")
+   }
+   else{
+      nav.classList.remove("scroll")
+   }
+})
+
+let toggler=document.querySelector(".navbar-toggler");
+
+toggler.onclick=function(){
+   nav.classList.toggle("top")
+}
